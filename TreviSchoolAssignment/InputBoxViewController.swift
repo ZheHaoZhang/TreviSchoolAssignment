d5d6c0492bdeef92092c99d66a2511189342155f
張哲豪 <ds73100@gmail.com>
2019-05-07 21:18:51 +0800

    new

---
+++ TreviSchoolAssignment/InputBoxViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InputBoxViewController.swift
+//  TreviSchoolAssignment
+//
+//  Created by 張哲豪 on 2019/5/7.
+//  Copyright © 2019 張哲豪. All rights reserved.
+//
+
+import UIKit
+
+class InputBoxViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
